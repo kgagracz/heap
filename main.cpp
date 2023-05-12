d85a4124ed48d0bd2e6c97bb6cf40e61df9605c4
Dateruri <37379748+Dateruri@users.noreply.github.com>
2023-05-12 16:18:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,66 +5,96 @@ using namespace std;
 
 class Heap {
 private:
-    vector<int> data;
+    vector<int> data;  // wektor przechowujący dane kopca
+
+    // Metoda do utrzymania własności kopca
     void heapify(int i) {
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-        int largest = i;
+        int left = 2 * i + 1;  // indeks lewego dziecka
+        int right = 2 * i + 2; // indeks prawego dziecka
+        int largest = i; // zakładamy, że największy element jest na pozycji i
+        // szukamy większego elementu wśród dziecka i rodzica
         if (left < data.size() && data[left] > data[largest]) {
             largest = left;
         }
         if (right < data.size() && data[right] > data[largest]) {
             largest = right;
         }
+        // jeśli największy element nie jest na pozycji i, to dokonujemy zamiany
         if (largest != i) {
             swap(data[i], data[largest]);
+            // ponieważ zamieniliśmy element, to musimy sprawdzić, czy poddrzewo jest kopcem
             heapify(largest);
         }
     }
+
 public:
-    //todo - dodać konstruktor przyjmujący {1,2,3,4...} tworzący z tego kopiec
+
+    // Domyślny konstruktor
     Heap() {}
+
+    // Konstruktor przyjmujący listę inicjalizacyjną
+    Heap(initializer_list<int> list) {
+        data = vector<int>(list); // inicjalizujemy wektor danymi z listy
+        // odtwarzamy własność kopca
+        for (int i = data.size() / 2 - 1; i >= 0; i--) {
+            heapify(i);
+        }
+    }
+
+    // Metoda do dodawania elementu do kopca
     void push(int val) {
-        data.push_back(val);
-        int i = data.size() - 1;
-        int parent = (i - 1) / 2;
+        data.push_back(val); // dodajemy element na koniec
+        int i = data.size() - 1; // indeks ostatniego elementu
+        int parent = (i - 1) / 2; // indeks rodzica
+        // odtwarzamy własność kopca po dodaniu nowego elementu
         while (i > 0 && data[parent] < data[i]) {
             swap(data[parent], data[i]);
             i = parent;
             parent = (i - 1) / 2;
         }
     }
+
+    // Metoda zwracająca element z wierzchołka kopca
     int top() {
         return data[0];
     }
+
+    // Metoda do usunięcia elementu z wierzchołka kopca
     void pop() {
-        data[0] = data.back();
-        data.pop_back();
-        heapify(0);
+        data[0] = data.back(); // przenosimy ostatni element na wierzchołek
+        data.pop_back(); // usuwamy ostatni element
+        heapify(0); // odtwarzamy własność kopca
     }
+
+    // Metoda sprawdzająca, czy kopiec jest pusty
     bool empty() {
         return data.empty();
     }
 
-    //operators overload
+    // Przeciążenie operatora == do porównywania kopców
     bool operator==(const Heap& other) const {
         return data == other.data;
     }
 
+    // Przeciążenie operatora != do porównywania kopców
     bool operator!=(const Heap& other) const {
         return !(*this == other);
     }
 
+    // Przeciążenie operatora + do dodawania kopców
     Heap operator+(const Heap& other) const {
-        Heap result = *this;
+        Heap result = *this; // tworzymy kopię obecnego kopca
+        // dodajemy wszystkie elementy z drugiego kopca
         for (int i : other.data) {
             result.push(i);
         }
         return result;
     }
 
+    // Przeciążenie operatora - do odejmowania kopców
     Heap operator-(const Heap& other) const {
-        Heap result = *this;
+        Heap result = *this; // tworzymy kopię obecnego kopca
+        // usuwamy wszystkie elementy, które są też w drugim kopcu
         for (int i : other.data) {
             auto it = find(result.data.begin(), result.data.end(), i);
             if (it != result.data.end()) {
@@ -75,6 +105,7 @@ public:
         return result;
     }
 
+    // Przeciążenie operatora << do wypisywania kopca
     friend std::ostream& operator<<(std::ostream& os, const Heap& heap) {
         for (const auto& value : heap.data) {
             os << value << " ";
@@ -83,6 +114,7 @@ public:
     }
 };
 
+// Funkcja do sortowania kopca
 void heapSort(vector<int>& arr) {
     Heap heap;
     for (int i : arr) {
@@ -96,42 +128,21 @@ void heapSort(vector<int>& arr) {
 }
 
 int main() {
-    vector<int> arr = {5, 2, 9, 1, 5, 6};
+
+    // Tworzymy kopiec z listy inicjalizacyjnej
+    Heap heap = { 5, 2, 9, 1, 5, 6 };
+
+    cout << "Original heap: " << heap << endl;
+
+    // Używamy funkcji heapSort do posortowania elementów kopca
+    vector<int> arr = { 5, 2, 9, 1, 5, 6 };
     heapSort(arr);
+
+    cout << "Sorted array: ";
     for (int i : arr) {
         cout << i << " ";
     }
     cout << endl;
 
-    // operator == presentation
-    Heap heap1;
-    for (int i : arr) {
-        heap1.push(i);
-    }
-
-    Heap heap2;
-    for (int i : arr) {
-        heap2.push(i);
-    }
-
-    if (heap1 == heap2) {cout << "Równe\n";}
-
-    //operator + presentation
-    Heap heap3 = heap1 + heap2;
-
-    // operator == presentation
-    if (heap3 != heap2) {cout << "Różne\n";}
-    cout << heap3;
-
     return 0;
 }
-
-//Kod implementuje sortowanie przez kopcowanie (heap sort), które korzysta z klas Heap oraz vector do przechowywania danych.
-//Metoda Heap::heapify jest prywatną metodą klasy Heap i służy do odtwarzania własności kopca dla wierzchołka w danym indeksie i, który może naruszać własność. Metoda ta znajduje indeks lewego i prawego syna wierzchołka i (jeśli istnieją), a następnie porównuje wartości tych wierzchołków z wartością wierzchołka i. Jeśli któryś z synów ma większą wartość niż wierzchołek i, to zamienia wartość wierzchołka i z wartością większego syna i wywołuje metodę heapify dla wierzchołka o nowym indeksie, który został zamieniony.
-//Metoda Heap::push dodaje nowy element do kopca i utrzymuje jego własność. W tym celu dodaje nowy element na końcu wektora i zamienia go z jego rodzicem, jeśli wartość rodzica jest mniejsza niż wartość dodawanego elementu, a następnie powtarza tę operację dla rodzica nowego elementu, dopóki nie zostanie przywrócona własność kopca.
-//Metoda Heap::top zwraca wartość elementu, który znajduje się na szczycie kopca.
-//Metoda Heap::pop usuwa element znajdujący się na szczycie kopca i przywraca jego własność poprzez wywołanie metody Heap::heapify na szczycie kopca.
-//Metoda Heap::empty zwraca true, jeśli kopiec jest pusty, a false w przeciwnym przypadku.
-//Funkcja heapSort przyjmuje wektor arr jako argument i tworzy obiekt klasy Heap. Następnie każdy element wektora arr jest dodawany do kopca za pomocą metody push klasy Heap. Następnie funkcja heapSort przenosi elementy kopca do wektora arr za pomocą metody top i usuwa je za pomocą metody pop, aż kopiec będzie pusty. Elementy wektora arr są ustawiane w odwrotnej kolejności, co zapewnia, że wektor arr będzie posortowany w kolejności rosnącej. Na końcu funkcja wypisuje posortowany wektor.
-//Funkcja main tworzy wektor arr i wywołuje funkcję heapSort, aby posortować jego elementy. Następnie wypisuje posortowany wektor na ekranie.
-//klasa jak typ predefiniowany, konstruktor, destruktor, przeciążenie operatorów
